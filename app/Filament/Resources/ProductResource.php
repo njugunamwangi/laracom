f94330a7d5698b06de1eb5a3111c3b17a815e4f2
njugunamwangi <njugunamwangi.n@gmail.com>
2023-10-18 23:27:14 +0300

    Code Refactor

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
 use Awcodes\Curator\Components\Forms\CuratorPicker;
+use Awcodes\Curator\Components\Tables\CuratorColumn;
 use Filament\Forms;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Section;
@@ -35,6 +36,7 @@ class ProductResource extends Resource
                                     ->maxLength(2000),
                                 Forms\Components\TextInput::make('slug')
                                     ->required()
+                                    ->hiddenOn('create')
                                     ->maxLength(2000),
                             ]),
                         Grid::make(2)
@@ -49,9 +51,11 @@ class ProductResource extends Resource
                         Grid::make(2)
                             ->schema([
                                 Forms\Components\TextInput::make('list_price')
+                                    ->prefix('Kes')
                                     ->required()
                                     ->numeric(),
                                 Forms\Components\TextInput::make('retail_price')
+                                    ->prefix('Kes')
                                     ->required()
                                     ->numeric(),
                             ]),
@@ -65,6 +69,7 @@ class ProductResource extends Resource
                         Grid::make(1)
                             ->schema([
                                 Forms\Components\Toggle::make('status')
+                                    ->label('Published')
                                     ->required(),
                             ]),
                         Forms\Components\RichEditor::make('warranty')
@@ -95,39 +100,51 @@ class ProductResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('image.name')
-                    ->numeric()
+                CuratorColumn::make('image_id')
+                    ->label('Image')
+                    ->height(50)
                     ->sortable(),
                 Tables\Columns\TextColumn::make('product')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('model_name')
+                    ->toggleable(isToggledHiddenByDefault: true)
                     ->searchable(),
                 Tables\Columns\TextColumn::make('model_number')
+                    ->toggleable(isToggledHiddenByDefault: true)
                     ->searchable(),
                 Tables\Columns\TextColumn::make('slug')
+                    ->toggleable(isToggledHiddenByDefault: true)
                     ->searchable(),
                 Tables\Columns\TextColumn::make('list_price')
+                    ->prefix('Kes ')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('retail_price')
+                    ->prefix('Kes ')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\IconColumn::make('status')
                     ->boolean(),
                 Tables\Columns\TextColumn::make('created_by')
+                    ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('updated_by')
+                    ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('deleted_by')
+                    ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('meta_title')
+                    ->toggleable(isToggledHiddenByDefault: true)
                     ->searchable(),
                 Tables\Columns\TextColumn::make('meta_description')
+                    ->toggleable(isToggledHiddenByDefault: true)
                     ->searchable(),
                 Tables\Columns\TextColumn::make('deleted_at')
+                    ->toggleable(isToggledHiddenByDefault: true)
                     ->dateTime()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('created_at')
