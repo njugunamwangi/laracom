dac1ae874667efc41eeedb8ed6804cae84049827
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-05 23:41:52 +0300

    Livewire: Add to cart

---
+++ app/Livewire/AddToCart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class AddToCart extends Component
+{
+    public function render()
+    {
+        return view('livewire.add-to-cart');
+    }
+}
--- resources/views/components/product/view.blade.php
+++ resources/views/components/product/view.blade.php
@@ -85,15 +85,7 @@
                     </div>
 
                     <!-- buttons - start -->
-                    <div class="flex gap-2.5">
-                        <a href="#" class="inline-block flex-1 rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 sm:flex-none md:text-base">Add to cart</a>
-
-                        <a href="#" class="inline-block rounded-lg bg-gray-200 px-4 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                            </svg>
-                        </a>
-                    </div>
+                    <livewire:add-to-cart />
                     <!-- buttons - end -->
                 </div>
             </div>
