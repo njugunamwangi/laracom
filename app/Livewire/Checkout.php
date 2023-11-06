c5d64abdd404aee052bbc4020ab1ee2ab99c41dd
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-07 00:46:37 +0300

    Checkout sub-totals

--- app/Livewire/Checkout.php
+++ app/Livewire/Checkout.php
@@ -7,7 +7,20 @@ use Livewire\Component;
 
 class Checkout extends Component
 {
-    public $cart, $subTotal = 0, $shipping = 1000;
+    public $cart, $subTotal, $shipping = 1000;
+
+    public function subTotal()
+    {
+        $this->cart = Cart::query()
+            ->where('user_id', '=', auth()->user()->id)
+            ->get();
+
+        foreach ($this->cart as $item) {
+            $this->subTotal += $item->product->retail_price * $item->quantity;
+        }
+
+        return $this->subTotal;
+    }
 
     public function render()
     {
@@ -15,8 +28,11 @@ class Checkout extends Component
             ->where('user_id', '=', auth()->user()->id)
             ->get();
 
+        $this->subTotal = $this->subTotal();
+
         return view('livewire.checkout', [
-            'cart' => $this->cart
+            'cart' => $this->cart,
+            'subTotal' => $this->subTotal
         ]);
     }
 }
