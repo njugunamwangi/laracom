be06fe08585d8e5373f94ce72e62d08d8bcb56fc
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-06 14:37:50 +0300

    Add product to cart

--- app/Livewire/AddToCart.php
+++ app/Livewire/AddToCart.php
@@ -2,12 +2,37 @@
 
 namespace App\Livewire;
 
+use App\Models\Cart;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class AddToCart extends Component
 {
+    public Product $product;
+
+    public function mount(Product $product) {
+        $this->product = $product;
+    }
     public function render()
     {
         return view('livewire.add-to-cart');
+    }
+
+    public function addToCart(int $productId) {
+        if (!Auth::check()) {
+            session()->flash('message', 'Please login to proceed.');
+        } else {
+
+            if ($this->product->where('id', $productId)->where('status', 1)->exists()) {
+                Cart::create([
+                    'user_id' => auth()->user()->id,
+                    'product_id' => $productId,
+                    'quantity' => 1
+                ]);
+            } else {
+                session()->flash('message', 'Product does not exist.');
+            }
+        }
     }
 }
--- resources/views/livewire/add-to-cart.blade.php
+++ resources/views/livewire/add-to-cart.blade.php
@@ -1,6 +1,9 @@
-<div class="flex gap-2.5">
-    <a href="#" class="inline-block flex-1 rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 sm:flex-none md:text-base">Add to cart</a>
+<button type="button"
+        wire:click="addToCart({{ $product->id }})"
+        href="#"
+        class="inline-block flex-1 rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 sm:flex-none md:text-base">
+        Add to cart
+</button>
 
 
-    <livewire:wishlist />
-</div>
+
