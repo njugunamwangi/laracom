0be5b45a58d0c295b6d8b88a1a923a1dbbe7c55b
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-07 00:47:58 +0300

    Form

--- resources/views/livewire/checkout.blade.php
+++ resources/views/livewire/checkout.blade.php
@@ -31,10 +31,9 @@
             </div>
 
             <div class="flex w-full justify-between border-t p-2 sm:w-auto sm:border-none sm:pl-0 lg:p-6 lg:pl-0">
-                <div class="ml-4 md:ml-8 md:pt-2 lg:ml-16">
+                <div class="ml-4 md:ml-8 md:pt-1 lg:ml-16">
                     <span class="block font-bold text-gray-800 md:text-lg">
                         Kes {{ number_format( $item->product->retail_price * $item->quantity, 2 ) }}
-                        @php $subTotal += $item->product->retail_price * $item->quantity @endphp
                     </span>
                 </div>
             </div>
