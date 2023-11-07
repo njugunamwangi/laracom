a280ecf7e748b910abd9a60028190909ea780af4
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-07 21:18:59 +0300

    Migration refactor

---
+++ database/migrations/2023_11_07_175545_add_tracking_no_to_orders_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('tracking_no', 2000);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('tracking_no');
+        });
+    }
+};
