e45de68a9b0348e3d2e56500b9aeee9c346ad5f5
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-08 00:58:45 +0300

    Relationships

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
@@ -18,4 +21,16 @@ class Order extends Model
         'payment_method',
         'payment_status',
     ];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function items(): HasMany {
+        return $this->hasMany(OrderItem::class, 'order_id', 'id');
+    }
+
+    public function details(): HasOne {
+        return $this->hasOne(OrderDetail::class, 'order_id', 'id');
+    }
 }
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OrderItem extends Model
 {
@@ -15,4 +16,8 @@ class OrderItem extends Model
         'quantity',
         'unit_price',
     ];
+
+    public function order(): BelongsTo {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
 }
