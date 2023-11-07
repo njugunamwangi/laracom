0e8fb0e14bf0e8f224af838b0e6463ba1de3709a
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-07 18:30:53 +0300

    Wishlist count

---
+++ app/Livewire/Wishlist/Count.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Livewire\Wishlist;
+
+use App\Models\Wishlist;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class Count extends Component
+{
+    public $wishlistCount;
+
+    public function wishlistCount() {
+        if (Auth::check()) {
+            return $this->wishlistCount = Wishlist::query()
+                ->where('user_id', '=', auth()->user()->id)
+                ->count();
+        } else {
+            return $this->wishlistCount = 0;
+        }
+    }
+
+    public function render()
+    {
+        $this->wishlistCount = $this->wishlistCount();
+
+        return view('livewire.wishlist.count', ['wishlistCount' => $this->wishlistCount]);
+    }
+}
