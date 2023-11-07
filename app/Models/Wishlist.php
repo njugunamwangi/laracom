4cddaaa8786712bb8e1bc6260f12ec7615e01c8c
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-07 19:02:20 +0300

    Product-Wishlist relationship

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Wishlist extends Model
 {
@@ -13,4 +14,9 @@ class Wishlist extends Model
         'user_id',
         'product_id',
     ];
+
+    public function product() : BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
 }
