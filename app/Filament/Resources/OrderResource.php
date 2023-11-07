b045895e1c98bae9e8fd972a7a03cedfac4029cb
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-08 00:58:17 +0300

    Infolist

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -7,6 +7,9 @@ use App\Filament\Resources\OrderResource\RelationManagers;
 use App\Models\Order;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Infolists\Components;
+use Filament\Infolists\Components\Tabs;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -17,7 +20,11 @@ class OrderResource extends Resource
 {
     protected static ?string $model = Order::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-bag';
+
+    protected static ?string $navigationGroup = 'Shop';
+    protected static ?int $navigationSort = 1;
+
 
     public static function form(Form $form): Form
     {
@@ -41,19 +48,101 @@ class OrderResource extends Resource
             ]);
     }
 
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Tabs::make('Label')
+                    ->tabs([
+                        Tabs\Tab::make('Order Summary')
+                            ->icon('heroicon-m-home-modern')
+                            ->schema([
+                                Components\Split::make([
+                                    Components\Grid::make(2)
+                                        ->schema([
+                                            Components\Group::make([
+                                                Components\TextEntry::make('user.name')
+                                                    ->label('Customer Name'),
+                                                Components\TextEntry::make('tracking_no')
+                                                    ->label('Tracking Number'),
+                                                Components\TextEntry::make('created_at')
+                                                    ->date(),
+
+                                            ]),
+                                            Components\Group::make([
+                                                Components\TextEntry::make('total_price')
+                                                    ->money('Kes'),
+                                                Components\TextEntry::make('payment_method'),
+                                                Components\TextEntry::make('payment_status')
+                                                    ->badge()
+                                                    ->color(fn (string $state): string => match ($state) {
+                                                        'Pending' => 'gray',
+                                                        'Processing' => 'warning',
+                                                        'Delivered' => 'success',
+                                                        'Cancelled' => 'danger',
+                                                    }),
+                                            ]),
+                                        ]),
+                                ])->from('lg'),
+                            ]),
+                        Tabs\Tab::make('Billing Information')
+                            ->icon('heroicon-m-adjustments-vertical')
+                            ->schema([
+                                Components\Split::make([
+                                    Components\Grid::make(2)
+                                        ->schema([
+                                            Components\Group::make([
+                                                Components\TextEntry::make('details.name')
+                                                    ->label('Name'),
+                                                Components\TextEntry::make('details.email')
+                                                    ->label('Email'),
+                                                Components\TextEntry::make('details.phone')
+                                                    ->label('Phone Number'),
+                                            ]),
+                                            Components\Group::make([
+                                                Components\TextEntry::make('details.address1')
+                                                    ->label('Address Line 1'),
+                                                Components\TextEntry::make('details.address2')
+                                                    ->label('Address Line 2'),
+                                                Components\TextEntry::make('details.city')
+                                                    ->label('City'),
+                                                Components\TextEntry::make('details.state')
+                                                    ->label('State'),
+                                                Components\TextEntry::make('details.zipcode')
+                                                    ->label('Zip Code'),
+                                            ]),
+                                        ]),
+                                ])->from('lg'),
+                            ]),
+                    ])->columnSpanFull(),
+            ]);
+    }
+
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('user_id')
-                    ->numeric()
+                Tables\Columns\TextColumn::make('tracking_no')
+                    ->label('Tracking Number')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->label('Customer')
+                    ->searchable()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('total_price')
+                    ->prefix('Kes ')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('payment_method')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('payment_status')
+                    ->badge()
+                    ->color(fn (string $state): string => match ($state) {
+                        'Pending' => 'gray',
+                        'Processing' => 'warning',
+                        'Delivered' => 'success',
+                        'Cancelled' => 'danger',
+                    })
                     ->searchable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
@@ -63,8 +152,6 @@ class OrderResource extends Resource
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('tracking_no')
-                    ->searchable(),
             ])
             ->filters([
                 Tables\Filters\TrashedFilter::make(),
@@ -81,14 +168,14 @@ class OrderResource extends Resource
                 ]),
             ]);
     }
-    
+
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-    
+
     public static function getPages(): array
     {
         return [
@@ -97,8 +184,8 @@ class OrderResource extends Resource
             'view' => Pages\ViewOrder::route('/{record}'),
             'edit' => Pages\EditOrder::route('/{record}/edit'),
         ];
-    }    
-    
+    }
+
     public static function getEloquentQuery(): Builder
     {
         return parent::getEloquentQuery()
