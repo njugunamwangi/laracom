023927b12c04bcfb99f88b70b06150bd48257e0e
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-08 01:09:58 +0300

    Relationships

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -20,4 +20,8 @@ class OrderItem extends Model
     public function order(): BelongsTo {
         return $this->belongsTo(Order::class, 'order_id');
     }
+
+    public function product(): BelongsTo {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
 }
