5991601eb28cf355b22546b1e8e28cfe84edee82
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-07 21:21:10 +0300

    Place order

--- app/Livewire/Checkout.php
+++ app/Livewire/Checkout.php
@@ -3,6 +3,10 @@
 namespace App\Livewire;
 
 use App\Models\Cart;
+use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Models\OrderItem;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class Checkout extends Component
@@ -16,8 +20,8 @@ class Checkout extends Component
         return [
             'name' => 'required|string|max:2000',
             'email' => 'required|email|max:2000',
-            'phone' => 'required|integer|min:10|max:12',
-            'address1' => 'required|integer|min:3|max:6',
+            'phone' => 'required|string|min:10|max:12',
+            'address1' => 'required|string|min:3|max:6',
             'city' => 'required|string|max:20',
             'state' => 'required|string|max:20',
             'zipcode' => 'required|string|min:4|max:7',
@@ -26,7 +30,44 @@ class Checkout extends Component
 
     public function placeOrder()
     {
-        $validatedData = $this->validate();
+        $this->validate();
+
+        $total = $this->subTotal + $this->shipping;
+
+        $order = Order::create([
+            'user_id' => auth()->user()->id,
+            'total_price' => $total,
+            'tracking_no' => Str::uuid(),
+            'payment_method' => 'Online',
+            'payment_status' => 'Pending',
+        ]);
+
+        foreach ($this->cart as $item) {
+            OrderItem::create([
+                'order_id' => $order->id,
+                'product_id' => $item->product_id,
+                'quantity' => $item->quantity,
+                'unit_price' => $item->product->retail_price,
+            ]);
+        }
+
+        $orderDetails = OrderDetail::create([
+            'order_id' => $order->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'address1' => $this->address1,
+            'address2' => $this->address2,
+            'city' => $this->city,
+            'state' => $this->state,
+            'zipcode' => $this->zipcode,
+        ]);
+
+        if ($order && $orderDetails) {
+            Cart::query()->where('user_id', '=', auth()->user()->id)->delete();
+
+            return redirect('/');
+        }
     }
 
     public function subTotal()
