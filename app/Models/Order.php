a8ab902d6c98c2cb8c6129a90fdf3ad8cfcfe42b
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-07 12:27:35 +0300

    Order model & migration

---
+++ app/Models/Order.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'total_price',
+        'payment_method',
+        'payment_status',
+    ];
+}
---
+++ database/migrations/2023_11_06_215205_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->decimal('total_price', 10, 2);
+            $table->string('payment_method', 45);
+            $table->string('payment_status', 45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
