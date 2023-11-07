bb450eef439ac28e18af025488d9457d025cf74d
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-07 16:02:58 +0300

    Form validation

--- app/Livewire/Checkout.php
+++ app/Livewire/Checkout.php
@@ -7,7 +7,27 @@ use Livewire\Component;
 
 class Checkout extends Component
 {
-    public $cart, $subTotal, $shipping = 1000;
+    public $cart, $subTotal = 0, $shipping = 1000;
+
+    public $name, $email, $phone, $address1, $address2, $city, $state, $zipcode;
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:2000',
+            'email' => 'required|email|max:2000',
+            'phone' => 'required|integer|min:10|max:12',
+            'address1' => 'required|integer|min:3|max:6',
+            'city' => 'required|string|max:20',
+            'state' => 'required|string|max:20',
+            'zipcode' => 'required|string|min:4|max:7',
+        ];
+    }
+
+    public function placeOrder()
+    {
+        $validatedData = $this->validate();
+    }
 
     public function subTotal()
     {
