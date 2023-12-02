cd1d187f1db670ee427a0d06b59f60234f45548d
njugunamwangi <njugunamwangi.n@gmail.com>
2023-12-02 03:02:02 +0300

    Titles

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -8,14 +8,20 @@ use Illuminate\Http\Request;
 class SiteController extends Controller
 {
     public function cart(): View {
-        return view('components.home.cart');
+        $title = 'Shopping Cart';
+
+        return view('components.home.cart', compact('title'));
     }
 
     public function checkout(): View {
-        return view('components.home.checkout');
+        $title = 'Checkout';
+
+        return view('components.home.checkout', compact('title'));
     }
 
     public function wishlist(): View {
-        return view('components.home.wishlist');
+        $title = 'Wishlist';
+
+        return view('components.home.wishlist', compact('title'));
     }
 }
