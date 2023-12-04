17343564fed32f981e6843936d99dcaf0631f5b6
njugunamwangi <njugunamwangi.n@gmail.com>
2023-12-04 18:43:13 +0300

    Composer update

--- config/database.php
+++ config/database.php
@@ -67,7 +67,7 @@ return [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
