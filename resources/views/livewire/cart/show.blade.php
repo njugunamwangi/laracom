11df6f84cbc8397033e471550d70f1cc69d4b3a3
njugunamwangi <njugunamwangi.n@gmail.com>
2023-12-06 16:39:54 +0300

    livewire navigate

--- resources/views/livewire/cart/show.blade.php
+++ resources/views/livewire/cart/show.blade.php
@@ -106,7 +106,7 @@
                 </div>
 
                 <a
-                    href="{{ route('checkout') }}"
+                    href="{{ route('checkout') }}" wire:navigate
                     class="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">
                     Check out
                 </a>
