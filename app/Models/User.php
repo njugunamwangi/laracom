914fcd863e35f9d40f495daa1edb01f2d4b924be
njugunamwangi <njugunamwangi.n@gmail.com>
2023-12-29 19:07:23 +0300

    user orders relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -62,5 +63,9 @@ class User extends Authenticatable
 
     public function payments(): HasMany {
         return $this->hasMany(Payment::class);
+    }
+
+    public function orders(): HasMany {
+        return $this->hasMany(User::class);
     }
 }
