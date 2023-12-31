73ac82b1a8539a49947510cd9930061c8f97aa96
njugunamwangi <njugunamwangi.n@gmail.com>
2023-12-31 19:30:53 +0300

    Viva payments

--- config/services.php
+++ config/services.php
@@ -31,4 +31,14 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'viva' => [
+        'api_key' => env('VIVA_API_KEY'),
+        'merchant_id' => env('VIVA_MERCHANT_ID'),
+        'environment' => env('VIVA_ENVIRONMENT', 'production'),
+        'client_id' => env('VIVA_CLIENT_ID'),
+        'client_secret' => env('VIVA_CLIENT_SECRET'),
+        'isv_partner_id' => env('VIVA_ISV_PARTNER_ID'),
+        'isv_partner_api_key' => env('VIVA_ISV_PARTNER_API_KEY'),
+    ],
+
 ];
