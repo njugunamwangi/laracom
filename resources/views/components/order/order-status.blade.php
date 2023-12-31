32d002443885213a78c7d72da1899bd908b06d4a
njugunamwangi <njugunamwangi.n@gmail.com>
2023-12-31 08:01:10 +0300

    Update order status

--- resources/views/components/order/order-status.blade.php
+++ resources/views/components/order/order-status.blade.php
@@ -1,9 +1,21 @@
 @if($status == 'Pending')
-    <span class="inline-flex items-center rounded-md bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600">Order {{ $status }}</span>
+    <span
+        class="inline-flex items-center rounded-md bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600">
+            Order {{ $status }}
+        </span>
 @elseif($status == 'Delivered')
-    <span class="inline-flex items-center rounded-md bg-green-100 px-2 py-1 text-xs font-medium text-green-700">Order {{ $status }}</span>
+    <span
+        class="inline-flex items-center rounded-md bg-green-100 px-2 py-1 text-xs font-medium text-green-700">
+            Order {{ $status }}
+        </span>
 @elseif($status == 'Processing')
-    <span class="inline-flex items-center rounded-md bg-blue-100 px-2 py-1 text-xs font-medium text-blue-700">Order {{ $status }}</span>
+    <span
+        class="inline-flex items-center rounded-md bg-blue-100 px-2 py-1 text-xs font-medium text-blue-700">
+            Order {{ $status }}
+        </span>
 @elseif($status == 'Cancelled')
-    <span class="inline-flex items-center rounded-md bg-red-100 px-2 py-1 text-xs font-medium text-red-700">Order {{$status}}</span>
+    <span
+        class="inline-flex items-center rounded-md bg-red-100 px-2 py-1 text-xs font-medium text-red-700">
+            Order {{$status}}
+        </span>
 @endif
