f93e56c579fb8434d6773410002fcd81c9bfb51e
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-12 22:05:34 +0300

    Admin user seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin',
+            'password' => bcrypt('password')
+        ]);
+
+        $user->assignRole(Role::IS_ADMIN);
+    }
+}
