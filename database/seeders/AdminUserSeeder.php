c69e991c74f6e9e5b050cf39e1e4b593361feefc
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-13 15:08:03 +0300

    Update README.md

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -16,7 +16,7 @@ class AdminUserSeeder extends Seeder
     {
         $user = User::create([
             'name' => 'Admin',
-            'email' => 'admin@admin',
+            'email' => 'admin@admin.com',
             'password' => bcrypt('password')
         ]);
 
