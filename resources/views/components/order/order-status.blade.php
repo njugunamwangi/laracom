2b37ddf86350d2bb5b6a97d0af0565077fb11784
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-13 15:37:07 +0300

    Bug fixes

--- resources/views/components/order/order-status.blade.php
+++ resources/views/components/order/order-status.blade.php
@@ -1,19 +1,19 @@
-@if($status == 'Pending')
+@if($status == App\Enums\OrderStatus::Pending)
     <span
         class="inline-flex items-center rounded-md bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600">
             Order {{ $status }}
         </span>
-@elseif($status == 'Delivered')
+@elseif($status == App\Enums\OrderStatus::Delivered)
     <span
         class="inline-flex items-center rounded-md bg-green-100 px-2 py-1 text-xs font-medium text-green-700">
             Order {{ $status }}
         </span>
-@elseif($status == 'Processing')
+@elseif($status == App\Enums\OrderStatus::Processing)
     <span
         class="inline-flex items-center rounded-md bg-blue-100 px-2 py-1 text-xs font-medium text-blue-700">
             Order {{ $status }}
         </span>
-@elseif($status == 'Cancelled')
+@elseif($status == App\Enums\OrderStatus::Cancelled)
     <span
         class="inline-flex items-center rounded-md bg-red-100 px-2 py-1 text-xs font-medium text-red-700">
             Order {{$status}}
