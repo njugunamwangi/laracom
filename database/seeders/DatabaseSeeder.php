8264f4361652cf6eeadd71e51272897b16f52441
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-15 13:01:37 +0300

    Fix role assignment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(RoleSeeder::class);
         $this->call(AdminUserSeeder::class);
         // \App\Models\User::factory(10)->create();
 
