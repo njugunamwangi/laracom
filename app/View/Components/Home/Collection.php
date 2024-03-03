72b68bf6ee90c8a87a8b034e32ef6dc406f2d583
njugunamwangi <njugunamwangi.n@gmail.com>
2024-03-03 10:34:11 +0300

    Collections

--- app/View/Components/Home/Collection.php
+++ app/View/Components/Home/Collection.php
@@ -2,6 +2,7 @@
 
 namespace App\View\Components\Home;
 
+use App\Models\Category;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
@@ -21,6 +22,11 @@ class Collection extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.home.collection');
+        $collections = Category::query()
+            ->whereNull('parent_id')
+            ->limit(3)
+            ->get();
+
+        return view('components.home.collection', compact('collections'));
     }
 }
