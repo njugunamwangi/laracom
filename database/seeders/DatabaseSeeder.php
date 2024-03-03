f3814a3acbf7512328e0d7102923ff63ad918076
njugunamwangi <njugunamwangi.n@gmail.com>
2024-03-03 18:26:13 +0300

    Register brand seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(AdminUserSeeder::class);
         $this->call(CategorySeeder::class);
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(BrandSeeder::class);
     }
 }
