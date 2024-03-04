757965be0ddb58a9e8a38c43a7341cedaac5092c
njugunamwangi <njugunamwangi.n@gmail.com>
2024-03-04 22:55:04 +0300

    Cart nav

--- resources/views/components/layout/navbar.blade.php
+++ resources/views/components/layout/navbar.blade.php
@@ -1,4 +1,136 @@
-<div x-data="{ category: {}, mobile: false, mobileCategory: {}, subCategory: {} }">
+<div x-data="{ category: {}, mobile: false, mobileCategory: {}, subCategory: {}, cart: false }">
+
+    <div x-show="cart" x-cloak @click.away="cart = false" class="relative z-10" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
+        <!--
+            Background backdrop, show/hide based on slide-over state.
+
+            Entering: "ease-in-out duration-500"
+            From: "opacity-0"
+            To: "opacity-100"
+            Leaving: "ease-in-out duration-500"
+            From: "opacity-100"
+            To: "opacity-0"
+        -->
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+        <div class="fixed inset-0 overflow-hidden">
+            <div class="absolute inset-0 overflow-hidden">
+            <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <!--
+                Slide-over panel, show/hide based on slide-over state.
+
+                Entering: "transform transition ease-in-out duration-500 sm:duration-700"
+                    From: "translate-x-full"
+                    To: "translate-x-0"
+                Leaving: "transform transition ease-in-out duration-500 sm:duration-700"
+                    From: "translate-x-0"
+                    To: "translate-x-full"
+                -->
+                <div class="pointer-events-auto w-screen max-w-md">
+                <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                    <div class="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+                    <div class="flex items-start justify-between">
+                        <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Shopping cart</h2>
+                        <div class="ml-3 flex h-7 items-center">
+                        <button @click="cart = !cart" type="button" class="relative -m-2 p-2 text-gray-400 hover:text-gray-500">
+                            <span class="absolute -inset-0.5"></span>
+                            <span class="sr-only">Close panel</span>
+                            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                        </div>
+                    </div>
+
+                    <div class="mt-8">
+                        <div class="flow-root">
+                        <ul role="list" class="-my-6 divide-y divide-gray-200">
+                            <li class="flex py-6">
+                            <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg" alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." class="h-full w-full object-cover object-center">
+                            </div>
+
+                            <div class="ml-4 flex flex-1 flex-col">
+                                <div>
+                                <div class="flex justify-between text-base font-medium text-gray-900">
+                                    <h3>
+                                    <a href="#">Throwback Hip Bag</a>
+                                    </h3>
+                                    <p class="ml-4">$90.00</p>
+                                </div>
+                                <p class="mt-1 text-sm text-gray-500">Salmon</p>
+                                </div>
+                                <div class="flex flex-1 items-end justify-between text-sm">
+                                <p class="text-gray-500">Qty 1</p>
+
+                                <div class="flex">
+                                    <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
+                                </div>
+                                </div>
+                            </div>
+                            </li>
+                            <li class="flex py-6">
+                            <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg" alt="Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch." class="h-full w-full object-cover object-center">
+                            </div>
+
+                            <div class="ml-4 flex flex-1 flex-col">
+                                <div>
+                                <div class="flex justify-between text-base font-medium text-gray-900">
+                                    <h3>
+                                    <a href="#">Medium Stuff Satchel</a>
+                                    </h3>
+                                    <p class="ml-4">$32.00</p>
+                                </div>
+                                <p class="mt-1 text-sm text-gray-500">Blue</p>
+                                </div>
+                                <div class="flex flex-1 items-end justify-between text-sm">
+                                <p class="text-gray-500">Qty 1</p>
+
+                                <div class="flex">
+                                    <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
+                                </div>
+                                </div>
+                            </div>
+                            </li>
+
+                            <!-- More products... -->
+                        </ul>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div class="border-t border-gray-200 px-4 py-6 sm:px-6">
+                    <div class="flex justify-between text-base font-medium text-gray-900">
+                        <p>Subtotal</p>
+                        <p>$262.00</p>
+                    </div>
+                    <p class="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+                    <div class="mt-6">
+                        <a
+                            href="{{ route('checkout') }}"
+                            wire:navigate
+                            class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
+                            Checkout
+                        </a>
+                    </div>
+                    <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
+                        <p>
+                        or
+                        <button @click="cart = false" type="button" class="font-medium text-indigo-600 hover:text-indigo-500">
+                            Continue Shopping
+                            <span aria-hidden="true"> &rarr;</span>
+                        </button>
+                        </p>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+
     <div x-show="mobile" x-cloak @click.away="mobile = false" class="relative z-40 lg:hidden" role="dialog" aria-modal="true">
         <!--
         Off-canvas menu backdrop, show/hide based on off-canvas menu state.
@@ -102,22 +234,22 @@
             <nav aria-label="Top">
                 <!-- Top navigation -->
                 <div class="bg-gray-900">
-                <div class="mx-auto flex h-10 max-w-7xl items-center justify-end px-4 sm:px-6 lg:px-8">
-                    <div class="flex items-center space-x-6">
-                        @if(!\Illuminate\Support\Facades\Auth::check())
-                            <a href="{{ route('login') }}" class="text-sm font-medium text-white hover:text-gray-100">Sign in</a>
-                            <a href="{{ route('register') }}" class="text-sm font-medium text-white hover:text-gray-100">Create an account</a>
-                        @else
-                            <a href="#" class="text-sm font-medium text-white hover:text-gray-100">Account</a>
-                            <form method="POST" action="{{ route('logout') }}" x-data>
+                    <div class="mx-auto flex h-10 max-w-7xl items-center justify-end px-4 sm:px-6 lg:px-8">
+                        <div class="flex items-center space-x-6">
+                            @if(!\Illuminate\Support\Facades\Auth::check())
+                                <a href="{{ route('login') }}" class="text-sm font-medium text-white hover:text-gray-100">Sign in</a>
+                                <a href="{{ route('register') }}" class="text-sm font-medium text-white hover:text-gray-100">Create an account</a>
+                            @else
+                                <a href="#" class="text-sm font-medium text-white hover:text-gray-100">Account</a>
+                                <form method="POST" action="{{ route('logout') }}" x-data>
 
-                                @csrf
+                                    @csrf
 
-                                <a href="{{ route('logout') }}" @click.prevent="$root.submit();" class="text-sm font-medium text-white hover:text-gray-100">Log Out</a>
-                            </form>
-                        @endif
+                                    <a href="{{ route('logout') }}" @click.prevent="$root.submit();" class="text-sm font-medium text-white hover:text-gray-100">Log Out</a>
+                                </form>
+                            @endif
+                        </div>
                     </div>
-                </div>
                 </div>
 
                 <!-- Secondary navigation -->
@@ -229,7 +361,7 @@
 
                             <!-- Cart -->
                             <div class="ml-4 flow-root lg:ml-8">
-                            <a href="{{ route('cart') }}" wire:navigate class="group -m-2 flex items-center p-2">
+                            <a @click="cart = !cart" class="group -m-2 flex items-center p-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 flex-shrink-0 text-white">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                                 </svg>
